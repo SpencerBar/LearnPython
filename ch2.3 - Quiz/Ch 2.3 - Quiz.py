46aebba922db328cb2724f36c50494147c43cbd1
SpencerBar <83757210+SpencerBar@users.noreply.github.com>
2021-05-10 20:02:47 -0400

    Add files via upload

---
+++ ch2.3 - Quiz/Ch 2.3 - Quiz.py
@@ -0,0 +1,20 @@
+while True:
+    spam = int(input())
+    if spam == 1:
+        print('Hello')
+    elif spam == 2:
+        print('Howdy')
+    elif spam == 0:
+        break
+    else:
+        print('Greetings')
+
+i = 0
+for i in range(10):
+    print (i+1)
+
+w = 0
+
+while w < 10:
+    print(w+1)
+    w += 1
