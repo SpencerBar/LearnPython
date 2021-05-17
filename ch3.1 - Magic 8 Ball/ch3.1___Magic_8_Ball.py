c12c4b3a4d3975f20c4e2917f650ee9b7f7563e3
SpencerBar <83757210+SpencerBar@users.noreply.github.com>
2021-05-16 20:07:07 -0400

    Update ch3.1___Magic_8_Ball.py

--- ch3.1 - Magic 8 Ball/ch3.1___Magic_8_Ball.py
+++ ch3.1 - Magic 8 Ball/ch3.1___Magic_8_Ball.py
@@ -1,24 +1,36 @@
 import random
 
-def GetAnswer(AnswerNumber):
-    if AnswerNumber == 1:
-        return 'It is certain'
-    elif AnswerNumber == 2:
-        return 'It is decidedly so'
-    elif AnswerNumber == 3:
-        return 'Yes'
-    elif AnswerNumber == 4:
-        return 'Reply hazy, try again'
-    elif AnswerNumber == 5:
-        return 'Ask again later'
-    elif AnswerNumber == 6:
-        return 'Concentrate and ask again later'
-    elif AnswerNumber == 7:
-        return 'My reply is no'
-    elif AnswerNumber == 8:
-        return 'Outlook is not good'
-    elif AnswerNumber == 9:
-        return 'Very Doubtful'
-    elif AnswerNumber == 10:
-        return 'No'
-print(getAnswer(random.randint(1, 9)))
+#def GetAnswer(AnswerNumber):
+ #   if AnswerNumber == 1:
+  #      return 'It is certain'
+  #  elif AnswerNumber == 2:
+  #      return 'It is decidedly so'
+  #  elif AnswerNumber == 3:
+  #      return 'Yes'
+  #  elif AnswerNumber == 4:
+  #      return 'Reply hazy, try again'
+ #   elif AnswerNumber == 5:
+ #       return 'Ask again later'
+ #   elif AnswerNumber == 6:
+ #       return 'Concentrate and ask again later'
+ #   elif AnswerNumber == 7:
+ #       return 'My reply is no'
+ #   elif AnswerNumber == 8:
+ #       return 'Outlook is not good'
+ #   elif AnswerNumber == 9:
+ #       return 'Very Doubtful'
+ #   elif AnswerNumber == 10:
+ #       return 'No'
+#print(getAnswer(random.randint(1, 9)))
+
+
+messages = ['It is certain',
+ 'It is decidedly so',
+ 'Yes definitely',
+ 'Reply hazy try again',
+ 'Ask again later',
+ 'Concentrate and ask again',
+ 'My reply is no',
+ 'Outlook not so good',
+ 'Very doubtful']
+print(messages[random.randint(0, len(messages) - 1)])
